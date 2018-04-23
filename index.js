a9c5cede216b75794116b41cf39fcbb7391ec23f
Alexey Belousov <belousov.fg@yandex.ru>
2018-04-23 11:09:17 +0300

    Fixed issue with process.stdout

--- index.js
+++ index.js
@@ -3,27 +3,8 @@ const fs = require('fs');
 
 const input = fs.createReadStream('input.txt');
 const output = fs.createWriteStream('output.txt');
+const hash = crypto.createHash('sha1').setEncoding('hex');
 
-input
-  .pipe(crypto.createHash('sha1').setEncoding('hex'))
-  .on('finish', function () {
-    let data = this.read();
-    console.log(data)
-    output.write(data);
-  });
-
-// Эта запись короче но не понятно как вывестиданные в консоль хеш
-// input
-//   .pipe(crypto.createHash('md5').setEncoding('hex'))
-//   .pipe(output);
-
-
-// Или так? но как получить в стриме записи данные чтобы вывести в консоль?
-// const hash = crypto.createHash('sha1').setEncoding('hex');
-//
-// input
-//   .pipe(hash)
-//   .pipe(output)
-//   .on('finish', function () {
-//     console.log(?);
-//   });
+input.pipe(hash);
+hash.pipe(output);
+hash.pipe(process.stdout);
